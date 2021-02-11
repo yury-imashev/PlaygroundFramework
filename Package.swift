5b74a228b682c81b1fa3702ede7be47fa03f3507
Yury Imashev <imashev@protonmail.com>
2021-02-11 13:54:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,17 +12,13 @@ let package = Package(
             targets: ["PlaygroundPackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PlaygroundPackage",
-            dependencies: []),
-        .testTarget(
-            name: "PlaygroundPackageTests",
-            dependencies: ["PlaygroundPackage"]),
+        .binaryTarget(
+            name: "PlaygroundFramework",
+            url: "https://github.com/someyura/PlaygroundFramework/blob/main/Release/PlaygroundFramework.xcframework.0.0.1.zip",
+            checksum: "ebfa030b0cef7aa4a41f82d494695357249e1f1dccc7e0f8c41013c177f8f09d")
     ]
 )
