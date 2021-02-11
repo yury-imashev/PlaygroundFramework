02840619c61cf121c76665f81f2e68cfc8dbe205
Yury Imashev <imashev@protonmail.com>
2021-02-11 17:01:31 +0100

    Add framework

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "PlaygroundFramework",
-            url: "https://github.com/someyura/PlaygroundFramework/blob/main/Release/PlaygroundFramework.xcframework.0.0.1.zip",
-            checksum: "4ed921c0a214a12c3b1df513742aad2294494e451e04649a23af09fa30391777")
+            path: "PlaygroundFramework.xcframework"
+        )
     ]
 )
