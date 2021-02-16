b4e201a8b415b35ad5ac509c67bb4cca48a32fd1
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2021-02-16 15:29:24 +0000

    Release 0.0.2

---
+++ UselessFramework.xcframework/ios-x86_64-simulator/UselessFramework.framework/Headers/UselessFramework.h
@@ -0,0 +1,18 @@
+//
+//  UselessFramework.h
+//  UselessFramework
+//
+//  Created by Yury Imashev on 16.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UselessFramework.
+FOUNDATION_EXPORT double UselessFrameworkVersionNumber;
+
+//! Project version string for UselessFramework.
+FOUNDATION_EXPORT const unsigned char UselessFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UselessFramework/PublicHeader.h>
+
+
