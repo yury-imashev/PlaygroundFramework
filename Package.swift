64226a91be333d4abef08b2897f40e34ec51cd1d
Yury Imashev <imashev@protonmail.com>
2021-02-17 09:59:15 +0100

    update dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PlaygroundPackage",
-            targets: ["PlaygroundFramework"]),
+            targets: ["UselessFramework"]),
     ],
     dependencies: [
     ],
@@ -17,8 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "PlaygroundFramework",
-            path: "PlaygroundFramework.xcframework"
+            name: "UselessFramework",
+            path: "UselessFramework.xcframework"
         )
     ]
 )
